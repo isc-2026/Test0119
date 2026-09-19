ae659e175060bda82852eab262aeaf98b1a62ae8
isc-2026 <information.001.005a@gmail.com>
2026-09-19 20:58:10 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
 <?php
-$name = "Inter School Competition";
-$number = 50 + 50;
+echo "🔥 PHP IS RUNNING!<br>";
+echo "Calculation: ".(50+50);
 ?>
-
-<!DOCTYPE html>
-<html>
-<body style="font-family:Arial;text-align:center;padding:50px">
-
-<h1>🔥 PHP TEST</h1>
-
-<h2>✅ PHP IS RUNNING!</h2>
-
-<p>Hello <?php echo $name; ?></p>
-
-<p>PHP Calculation: <?php echo $number; ?></p>
-
-<p>PHP Version: <?php echo phpversion(); ?></p>
-
-</body>
-</html>
