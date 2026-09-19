2ab9b97ffcf4923d9701de39607ef33c8a23cc10
isc-2026 <information.001.005a@gmail.com>
2026-09-19 20:52:12 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$name = "Inter School Competition";
+$number = 50 + 50;
+?>
+
+<!DOCTYPE html>
+<html>
+<body style="font-family:Arial;text-align:center;padding:50px">
+
+<h1>🔥 PHP TEST</h1>
+
+<h2>✅ PHP IS RUNNING!</h2>
+
+<p>Hello <?php echo $name; ?></p>
+
+<p>PHP Calculation: <?php echo $number; ?></p>
+
+<p>PHP Version: <?php echo phpversion(); ?></p>
+
+</body>
+</html>
